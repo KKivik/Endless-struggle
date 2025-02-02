5ffb4c3b1a6af0e03e7a31c8d92c590144fe0a17
KKivik <ivanov_lab@list.ru>
2025-02-02 21:32:33 +0200

    Working_with_camera_3
    The camera is not working properly, but there is not much work left

--- camera.py
+++ camera.py
@@ -2,14 +2,24 @@ from settings import *
 class Camera(pygame.sprite.Group):
     def __init__(self, map):
         super().__init__()
+
+        # camera offset
         self.display_surfsce = pygame.display.get_surface()
-        #camera offset
-        self.offset = pygame.math.Vector2(500, 100)
+        self.half_w = self.display_surfsce.get_size()[0] // 2
+        self.half_h = self.display_surfsce.get_size()[1] // 2
+
+        self.offset = pygame.math.Vector2()
         self.map = map
         self.map_rect = self.map.get_rect()
 
-    def custom_draw(self):
+    def center_camera_target(self, target):
+        self.offset.x = target.rect.centerx - self.half_w
+        self.offset.y = target.rect.centery - self.half_h
+
+
+    def custom_draw(self, person):
         map_offset = self.map_rect + self.offset
+        self.center_camera_target(person)
         self.map.render(map_offset)
         for sprite in self.sprites():
             offset_pos = sprite.rect.topleft + self.offset
--- main.py
+++ main.py
@@ -21,13 +21,21 @@ class Person(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = width / 2 - 30
         self.rect.y = height / 2 - 30
-        self.sign = 1
+        self.speed = 5
 
-    def update(self):
-        pass
+    def update(self, *args):
+        if args and args[0].type == pygame.KEYDOWN:
+            if args[0].key == pygame.K_RIGHT:
+                self.rect = self.rect.move(10, 0)
+            if args[0].key == pygame.K_LEFT:
+                self.rect = self.rect.move(-10, 0)
+            if args[0].key == pygame.K_DOWN:
+                self.rect = self.rect.move(0, 10)
+            if args[0].key == pygame.K_UP:
+                self.rect = self.rect.move(0, -10)
 
 if __name__ == '__main__':
-    fps = 50  # кадр/с
+    fps = 60  # кадр/с
 
     clock = pygame.time.Clock()
     running = True
@@ -39,12 +47,11 @@ if __name__ == '__main__':
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
+            all_sprites.update(event)
 
         screen.fill((0, 0, 0))
-        map.render(screen)
-        all_sprites.custom_draw()
-        all_sprites.update()
+        all_sprites.custom_draw(Main_Person)
+
         pygame.display.flip()
         clock.tick(fps)
     pygame.quit()
