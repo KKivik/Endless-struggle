5410189024dcdbac993cf9e5e89ef448e6df0089
KKivik <ivanov_lab@list.ru>
2025-04-09 11:07:44 +0200

    comm

--- data/assets1.tsx
+++ data/assets1.tsx
@@ -1,4 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="assets1" tilewidth="32" tileheight="32" tilecount="210" columns="10">
  <image source="../../../../Хлам/TilesetGrass/assets1.png" width="336" height="672"/>
+ <wangsets>
+  <wangset name="Безымянный набор" type="edge" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+  </wangset>
+ </wangsets>
 </tileset>
